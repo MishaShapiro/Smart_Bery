0359b8f8617e3a971cdaf36d31544a33c2cf07e9
MishaShapiro <112972631+MishaShapiro@users.noreply.github.com>
2024-06-02 17:14:36 +0300

    Add files via upload

--- first_task.js
+++ first_task.js
@@ -1,17 +1,25 @@
 function printWords(dict) {
     let res = ""
-    
-    function Finder(dict, tempRes) {
-        for (let key in dict) {
-            if (dict[key]) {
-                Finder(dict[key], tempRes + key)
-            } else {
-                res += tempRes + key + " "
-                return
-            }
+    function Words(dict, part) {
+      let next_dict = dict
+      while (next_dict) {
+        let keys = Object.keys(next_dict)
+        if (keys.length === 1) {
+          next_dict = next_dict[keys[0]]
+          part += keys[0]
+          if (!next_dict) {
+            res += part + " "
+            return
+          }
+        } else {
+          for (let key of keys) {
+            Words(next_dict[key], part + key)
+          }
+          return
         }
+      }
     }
-
-    Finder(dict, "")
+  
+    Words(dict, "")
     return res.slice(0, res.length - 1)
-}
+  }
